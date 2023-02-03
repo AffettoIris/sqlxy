ca82d76dee29546dfec3751cea07576947d449af
AfetoIris <103482977+AfetoIris@users.noreply.github.com>
2023-02-04 01:59:10 +0800

    hidden password

--- static/common/php/stuConn.php
+++ static/common/php/stuConn.php
@@ -3,7 +3,7 @@
 //student对sqlxy.answer all privilege、对envxy all privilege;
     $host = '47.115.218.216';
     $username = 'student';
-    $password = 'zhang134679';
+    $password = '这是密码，付费观看！';
     $dbname = 'envxy';
     //    $sConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     //    $sConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
