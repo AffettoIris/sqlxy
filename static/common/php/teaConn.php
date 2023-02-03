398c7620507da490de3c641cfa8a9fc7a967379e
AfetoIris <103482977+AfetoIris@users.noreply.github.com>
2023-02-04 01:59:24 +0800

    Update teaConn.php

--- static/common/php/teaConn.php
+++ static/common/php/teaConn.php
@@ -3,7 +3,7 @@
 // teacher 对sqlxy.answer和sqlxy.exercise和sqlxy.student和sqlxy.teacher和envxy all privilege
     $host = '47.115.218.216';
     $username = 'teacher';
-    $password = 'zhang134679';
+    $password = '这是密码，付费观看！';
     $dbname = 'envxy'; // 默认数据库为envxy，用于老师给题目初始化
     //    $tConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     //    $tConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
